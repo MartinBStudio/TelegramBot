218f76f9f08ddc2ba81b3234371f9ace31a1268e
Martin Ma <martin.masika@icloud.com>
2024-09-21 18:50:20 +0200

    init

--- src/main/java/cz/raadost/TelegramBot.java
+++ src/main/java/cz/raadost/TelegramBot.java
@@ -1,13 +1,12 @@
-package cz.hookapp;
+
+package cz.raadost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hookapp {
-
+public class TelegramBot {
 	public static void main(String[] args) {
-		SpringApplication.run(Hookapp.class, args);
+		SpringApplication.run(TelegramBot.class, args);
 	}
-
 }
---
+++ src/test/java/cz/raadost/UnitTests.java
@@ -0,0 +1,11 @@
+package cz.raadost;
+
+import org.testng.annotations.Test;
+
+public class UnitTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("asd");
+	}
+}
